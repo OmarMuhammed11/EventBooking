979d9a17478342bd5214253e1fbb2147ce65ba8a
OmarMuhammed11 <alrsheed.omar1@gmail.com>
2024-02-11 21:24:13 +0300

    Secoend ver

--- bookproject/bookwebsite/apps/bookmodule/urls.py
+++ bookproject/bookwebsite/apps/bookmodule/urls.py
@@ -19,5 +19,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index,name='index')
+    path("",views.index,name='index'),
+    path("books/",views.books)
+
+    
 ]
--- bookproject/bookwebsite/apps/bookmodule/views.py
+++ bookproject/bookwebsite/apps/bookmodule/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+
 def index(request):
-    return render(request,'bookmodule\index.html')
+    return render(request,'bookmodule/index.html')
+
+
+def books(request):
+    return render(request,'bookmodule/books.html')
 
 # Create your views here.

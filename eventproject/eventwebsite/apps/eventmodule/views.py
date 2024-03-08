d4ec27c60c78670717bd432a2616e82518bc17ca
OmarMuhammed11 <alrsheed.omar1@gmail.com>
2024-03-09 01:32:09 +0300

    6th

--- eventproject/eventwebsite/apps/eventmodule/views.py
+++ eventproject/eventwebsite/apps/eventmodule/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render,redirect
 
 def index(request):
+   
    return render(request, 'eventmodule/home_page.html')
+
+def event_detil(request):
+   return render(request, 'eventmodule/eventd_page.html')

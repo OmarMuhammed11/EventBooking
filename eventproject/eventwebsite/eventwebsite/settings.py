0aa4d92f1ad25d65037b9318d83fa617234d9b4b
OmarMuhammed11 <alrsheed.omar1@gmail.com>
2024-05-05 12:27:28 +0300

    last v

--- eventproject/eventwebsite/eventwebsite/settings.py
+++ eventproject/eventwebsite/eventwebsite/settings.py
@@ -120,8 +120,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR,'apps/statics/media'),
+]
 
-STATIC_URL = 'static/'
+STATIC_URL = 'statics/'
+STATIC_ROOT = os.path.join(BASE_DIR,"apps"+os.sep+"statics")
+#MEDIA_URL = 'media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR,"apps"+os.sep+"statics"+os.sep+"media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
